b64db8226a161af21bf5a408a04ce13489a5f5b2
Prince Vasconcelos <prince.vasconcelos@confitec.com.br>
2021-03-22 14:06:33 -0300

    create Details page

---
+++ src/pages/Details/index.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { useParams, Link } from "react-router-dom";
+
+import DataContext from 'contexts/data'
+import API from "service";
+import Loading from 'components/Loading'
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+export default () => {
+    const [data, setData] = useState({})
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const { pokemons, saveEndpointResult, savePokemonData, buyPokemon } = useContext(DataContext)
+    const { name } = useParams();
+    const pokeDetailEndpoint = 'https://pokeapi.co/api/v2/pokemon/'+name
+
+    const handleBuyButton = () => buyPokemon(name)
+
+    useEffect(() => {
+        if (!name) return
+
+        const cacheData = pokemons[name]
+        if (cacheData) return setData(cacheData)
+
+        getPokemonDetails()
+    }, [name])
+
+    const getPokemonDetails = async () => {
+        setLoading(true);
+    
+        try {
+            const pokemon = await API.get(pokeDetailEndpoint)
+            
+            if (!pokemon) return setError(`Nenhum resultado encontrado para "${name}"`)
+
+            setData(pokemon);
+            saveEndpointResult({
+                url: pokeDetailEndpoint,
+                result: pokemon,
+            })
+            savePokemonData({
+                [name]: pokemon
+            })
+        } catch (err) {
+            console.log('prince errr', err.response)
+            if (err.response.status === 404) {
+                setError(`Nenhum resultado encontrado para  "${name}"`)
+            }
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) return <Loading />
+
+    if (!name) return <S.Message>Pesquise o nome do pokemon no pesquisar acima</S.Message>
+
+    if (error) return <S.Message>{error}</S.Message>
+
+    if (Object.keys(data).length === 0) return <S.Message>Não existem informações para esse pokemon</S.Message>
+
+    const { stats, moves, abilities, sprites} = data
+    console.log('prince', {
+        stats, moves, abilities, sprites
+    })
+    return (
+        <S.DetailsContainer>
+            <Link to="/">Voltar</Link>
+            <S.ImageContainer>
+                <img src={sprites.other.dream_world.front_default} />
+                <S.Heading>{name}</S.Heading>
+                <Button onClick={handleBuyButton}>Comprar</Button>
+            </S.ImageContainer>
+            <S.TextsContainer>
+                <S.List>
+                    <h3>Informações</h3>
+                    <ul>
+                        {stats.map(({ base_stat, stat}) => (
+                            <li key={stat.name}>
+                                <span>{stat.name}</span>
+                                <span>{base_stat}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </S.List>
+
+                <S.List>
+                    <h3>Poderes</h3>
+                    <ul>
+                        {moves.map(({ move, stat}) => (
+                            <li key={move.name}>
+                                <span>{move.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </S.List>
+
+                <S.List>
+                    <h3>Habilidades</h3>
+                    <ul>
+                        {abilities.map(({ ability}) => (
+                            <li key={ability.name}>
+                                <span>{ability.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </S.List>
+            </S.TextsContainer>
+        </S.DetailsContainer>
+    )
+
+}
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const DetailsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 24px 0;
+
+    h1 {
+        font-size: 64px;
+    }
+
+    > a {
+        margin-left: 24px;
+        font-weight: 700;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`
+
+export const Message = styled.span`
+    font-size: 20px;
+`
+
+export const Heading = styled.h1`
+    text-transform: capitalize;
+    width: 100%;
+`
+
+export const ImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    button {
+        margin: 24px 0 0;
+    }
+`
+
+export const List = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-height: 500px;
+
+    ul {
+        border: 2px solid black;
+        padding: 8px;
+        overflow: auto;
+    }
+
+
+    li {
+        min-width: 200px;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 16px;
+
+        span {
+            text-transform: capitalize;
+        }
+    }
+`
+
+export const TextsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    margin-top: 32px;
+
+    @media (min-width: 600px) {
+        flex-direction: row;
+    }
+
+    > div {
+        margin: 16px 0;
+    }
+`
