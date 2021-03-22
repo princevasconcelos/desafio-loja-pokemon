a8ae80f10e4239120c8cba9e60b4ed7baa11d3d2
Prince Vasconcelos <prince.vasconcelos@confitec.com.br>
2021-03-22 14:08:07 -0300

    create global style

---
+++ src/GlobalStyles.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+import "@fontsource/montserrat";
+
+export default createGlobalStyle`
+	*,
+	*::before,
+	*::after {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+    body {
+        font-family: 'Montserrat';
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+	}
+
+	a {
+		text-decoration: none;
+		color: #333;
+	}
+
+	button {
+		cursor: pointer;
+        border: none;
+		outline: none;
+		background: transparent;
+		font-size: 16px;
+	}
+
+	h1,h2,h3 {
+		text-align: center;
+	}
+
+    ul {
+        list-style: none;
+    }
+`;
