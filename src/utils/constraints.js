bf1eb149140aa80fa6be291dbedc22630c37b544
Prince Vasconcelos <prince.vasconcelos@confitec.com.br>
2021-03-22 14:05:48 -0300

    create helpers and constraints

---
+++ src/utils/constraints.js
@@ -0,0 +1,9 @@
+export const typeMapPT_EN = type => ({
+    fogo: 'fire',
+    agua: 'water',
+    grama: 'grass',
+    eletrico: 'electric',
+    inseto: 'bug',
+    dragao: 'dragon',
+    fantasma: 'ghost'
+})[type]
